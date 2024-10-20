40be683fb4cf706cec45e5ef2b955f01348b23c6
jonasaacampos <jonasaacampos@gmail.com>
2024-10-20 09:18:24 -0300

    feat: generate farm chart

--- produtor_rural/urls.py
+++ produtor_rural/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import dashboard, generate_chart, generate_pie_chart, home, generate_pie_chart_culturas, generate_pie_chart_solo_usage
+from .views import dashboard, generate_bar_chart_farms_per_state, generate_pie_chart, home, generate_pie_chart_culturas, generate_pie_chart_solo_usage
 from .autocomplete import CidadeAutocomplete
 
 urlpatterns = [
     path("", home, name="home"),
     path("dashboard/", dashboard, name="dashboard"),
     path("cidade-autocomplete/", CidadeAutocomplete.as_view(), name="cidade-autocomplete"),
-    path("generate_chart/", generate_chart, name="generate_chart"),
+    path("generate_bar_chart_farms_per_state/", generate_bar_chart_farms_per_state, name="generate_bar_chart_farms_per_state"),
     path("generate_pie_chart/", generate_pie_chart, name="generate_pie_chart"),
     path("generate_pie_chart_culturas/", generate_pie_chart_culturas, name="generate_pie_chart_culturas"),
     path("generate_pie_chart_solo_usage/", generate_pie_chart_solo_usage, name="generate_pie_chart_solo_usage"),
--- produtor_rural/views.py
+++ produtor_rural/views.py
@@ -11,105 +11,162 @@ from django.http import HttpResponse
 import io
 import base64
 import random
+from time import sleep
 
 # Use o backend 'Agg' para evitar problemas com GUI
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 
 
 def dashboard(request):
     data = fetch_dashboard_data()
-    return render(request, 'produtor/dashboard.html', data)
+    return render(request, "produtor/dashboard.html", data)
+
 
 def home(request):
     data = fetch_dashboard_data()
-    return render(request, 'home.html', data)
+    return render(request, "home.html", data)
 
-def generate_chart(request):
+
+############################################################################################################
+#                                              Gráficos                                                    #
+############################################################################################################
+
+
+
+
+def generate_bar_chart_farms_per_state(request):
     estado_count = get_estado_count()
     df = pd.DataFrame(estado_count)
 
     # Gerar cores aleatórias para cada barra
-    colors = ['#%06X' % random.randint(0, 0xFFFFFF) for _ in range(len(df))]
+    colors = ["#%06X" % random.randint(0, 0xFFFFFF) for _ in range(len(df))]
 
-    plt.figure(figsize=(10, 5))
-    plt.bar(df['estado'], df['count'], color=colors)
-    plt.title('Gráfico por Estado')
-    plt.xlabel('Estado')
-    plt.ylabel('Count')
+    plt.figure(figsize=(8, 8))
+    plt.bar(df["estado"], df["count"], color=colors)
+    plt.title("Gráfico por Estado")
+    plt.xlabel("Estado")
+    plt.ylabel("Count")
 
     buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
+    plt.savefig(buffer, format="png")
     buffer.seek(0)
-    return HttpResponse(buffer, content_type='image/png')
+    return HttpResponse(buffer, content_type="image/png")
+
 
 def generate_pie_chart(request):
     estado_count = get_estado_count()
     df = pd.DataFrame(estado_count)
 
-    plt.figure(figsize=(10, 5))
-    plt.pie(df['count'], labels=df['estado'], autopct='%1.1f%%')
-    plt.title('Gráfico por Estado')
+    plt.figure(figsize=(8, 8))
+    plt.pie(df["count"], labels=df["estado"], autopct="%1.1f%%")
+    plt.title("Gráfico por Estado")
 
     buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
+    plt.savefig(buffer, format="png")
     buffer.seek(0)
-    return HttpResponse(buffer, content_type='image/png')
+    return HttpResponse(buffer, content_type="image/png")
 
 
 def generate_pie_chart_culturas(request):
     cultura_count = get_cultura_count()
-    
+
     # print("Tipo de cultura_count:", type(cultura_count))
     # print("Formato de cultura_count:", cultura_count)
-    
-    if not cultura_count:
-        return HttpResponse("No data available", content_type='text/plain')
 
-    if not isinstance(cultura_count, list) or not all(isinstance(item, dict) for item in cultura_count):
-        return HttpResponse("Invalid data format", content_type='text/plain')
-    if not all('culturas' in item and 'count' in item for item in cultura_count):
-        return HttpResponse("Invalid data format", content_type='text/plain')
+    if not cultura_count:
+        return HttpResponse("No data available", content_type="text/plain")
+
+    if not isinstance(cultura_count, list) or not all(
+        isinstance(item, dict) for item in cultura_count
+    ):
+        return HttpResponse("Invalid data format", content_type="text/plain")
+    if not all("culturas" in item and "count" in item for item in cultura_count):
+        return HttpResponse("Invalid data format", content_type="text/plain")
 
     df = pd.DataFrame(cultura_count)
 
-    plt.figure(figsize=(10, 5))
-    plt.pie(df['count'], labels=df['culturas'], autopct='%1.1f%%')
-    plt.title('Gráfico de Culturas Cultivadas')
+    plt.figure(figsize=(8, 8))
+    plt.pie(df["count"], labels=df["culturas"], autopct="%1.1f%%")
+    plt.title("Gráfico de Culturas Cultivadas")
 
     buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
+    plt.savefig(buffer, format="png")
     plt.close()  # Fechar a figura para liberar memória
     buffer.seek(0)
-    return HttpResponse(buffer, content_type='image/png')
+    return HttpResponse(buffer, content_type="image/png")
+
+
+import matplotlib.pyplot as plt
+import io
+from django.http import HttpResponse
 
 
 def generate_pie_chart_solo_usage(request):
-    solo_usage = get_solo_usage_count
-    
-    df = pd.DataFrame(solo_usage)
+    # Exemplo de dados
+    solo_agricultavel = get_solo_agricultavel_count()
+    solo_preservado = get_solo_preservado_count()
 
-    plt.figure(figsize=(10, 5))
-    plt.pie(df['count'], labels=df['Tipo Solo'], autopct='%1.1f%%')
-    plt.title('Gráfico de Uso do Solo')
+    # Calcular as áreas totais em hectares
+    total_agricultavel = sum(item["area_agricultavel"] for item in solo_agricultavel)
+    total_preservado = sum(item["area_vegetacao"] for item in solo_preservado)
 
+    # Rótulos e tamanhos para o gráfico de pizza
+    labels = ["Área Agricultável", "Área Preservada"]
+    sizes = [total_agricultavel, total_preservado]
+
+    # Gerar o gráfico de pizza
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.pie(sizes, labels=labels, autopct="%1.1f%%", colors=["#FF6384", "#36A2EB"])
+    ax.set_title("Comparação de Uso do Solo em Hectares")
+    ax.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    # Salvar o gráfico em um buffer
     buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
-    plt.close() 
+    plt.savefig(buffer, format="png")
+    plt.close(fig)
     buffer.seek(0)
-    return HttpResponse(buffer, content_type='image/png')
 
+    return HttpResponse(buffer, content_type="image/png")
+
+
+############################################################################################################
+#                                           Funções auxiliares                                             #
+############################################################################################################
 
 
 def get_estado_count():
-    queryset = Fazenda.objects.values('estado').annotate(count=Count('estado')).order_by('estado')
+    queryset = (
+        Fazenda.objects.values("estado")
+        .annotate(count=Count("estado"))
+        .order_by("estado")
+    )
     return list(queryset)
+
 
 def get_cultura_count():
-    queryset = Fazenda.objects.values('culturas').annotate(count=Count('culturas')).order_by('culturas')
+    queryset = (
+        Fazenda.objects.values("culturas")
+        .annotate(count=Count("culturas"))
+        .order_by("culturas")
+    )
     return list(queryset)
 
-def get_solo_usage_count():
-    queryset = Fazenda.objects.values('uso_solo').annotate(count=Count('uso_solo')).order_by('uso_solo')
+
+def get_solo_agricultavel_count():
+    queryset = (
+        Fazenda.objects.values("area_agricultavel")
+        .annotate(count=Count("area_agricultavel"))
+        .order_by("area_agricultavel")
+    )
+    return list(queryset)
+
+
+def get_solo_preservado_count():
+    queryset = (
+        Fazenda.objects.values("area_vegetacao")
+        .annotate(count=Count("area_vegetacao"))
+        .order_by("area_vegetacao")
+    )
     return list(queryset)
 
 
@@ -120,7 +177,7 @@ def get_solo_usage_count():
 #     area_total = fazendas.aggregate(Sum('area_total'))['area_total__sum']
 #     area_agricultavel = fazendas.aggregate(Sum('area_agricultavel'))['area_agricultavel__sum']
 #     area_vegetacao = fazendas.aggregate(Sum('area_vegetacao'))['area_vegetacao__sum']
-  
+
 #     estado_count = fazendas.values('estado').annotate(count=Count('estado'))
 #     cultura_count = fazendas.values('culturas').annotate(count=Count('culturas'))
 #      # QuerySet para contagem de estados
@@ -129,10 +186,10 @@ def get_solo_usage_count():
 #     estado_count = list(estado_count_queryset)
 #     context = {
 #         'estado_count': estado_count,
-#         'tipo_estado_count': type(estado_count).__name__, 
+#         'tipo_estado_count': type(estado_count).__name__,
 #     }
-  
-#     return render(request, 'produtor/dashboard.html', {  
+
+#     return render(request, 'produtor/dashboard.html', {
 #         'produtores': produtores,
 #         'fazendas': fazendas,
 #         'total_fazendas': total_fazendas,
