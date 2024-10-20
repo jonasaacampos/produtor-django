aff3dcd47106c1c8846953370bf77a4ea27e3733
jonasaacampos <jonasaacampos@gmail.com>
2024-10-20 09:24:26 -0300

    clean: clean code write, renaming variables, inserte comments marks and removed old blocks comented codes

--- produtor_rural/urls.py
+++ produtor_rural/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dashboard, generate_bar_chart_farms_per_state, generate_pie_chart, home, generate_pie_chart_culturas, generate_pie_chart_solo_usage
+from .views import dashboard, generate_bar_chart_farms_per_state, generate_pie_chart_farms_per_state, home, generate_pie_chart_culturas, generate_pie_chart_solo_usage
 from .autocomplete import CidadeAutocomplete
 
 urlpatterns = [
@@ -7,7 +7,7 @@ urlpatterns = [
     path("dashboard/", dashboard, name="dashboard"),
     path("cidade-autocomplete/", CidadeAutocomplete.as_view(), name="cidade-autocomplete"),
     path("generate_bar_chart_farms_per_state/", generate_bar_chart_farms_per_state, name="generate_bar_chart_farms_per_state"),
-    path("generate_pie_chart/", generate_pie_chart, name="generate_pie_chart"),
+    path("generate_pie_chart_farms_per_state/", generate_pie_chart_farms_per_state, name="generate_pie_chart_farms_per_state"),
     path("generate_pie_chart_culturas/", generate_pie_chart_culturas, name="generate_pie_chart_culturas"),
     path("generate_pie_chart_solo_usage/", generate_pie_chart_solo_usage, name="generate_pie_chart_solo_usage"),
     
--- produtor_rural/views.py
+++ produtor_rural/views.py
@@ -11,7 +11,6 @@ from django.http import HttpResponse
 import io
 import base64
 import random
-from time import sleep
 
 # Use o backend 'Agg' para evitar problemas com GUI
 matplotlib.use("Agg")
@@ -31,8 +30,7 @@ def home(request):
 #                                              Gráficos                                                    #
 ############################################################################################################
 
-
-
+# Fazendas por Estado
 
 def generate_bar_chart_farms_per_state(request):
     estado_count = get_estado_count()
@@ -53,7 +51,7 @@ def generate_bar_chart_farms_per_state(request):
     return HttpResponse(buffer, content_type="image/png")
 
 
-def generate_pie_chart(request):
+def generate_pie_chart_farms_per_state(request):
     estado_count = get_estado_count()
     df = pd.DataFrame(estado_count)
 
@@ -66,6 +64,8 @@ def generate_pie_chart(request):
     buffer.seek(0)
     return HttpResponse(buffer, content_type="image/png")
 
+
+# Gráfico de Culturas Cultivadas
 
 def generate_pie_chart_culturas(request):
     cultura_count = get_cultura_count()
@@ -96,10 +96,7 @@ def generate_pie_chart_culturas(request):
     return HttpResponse(buffer, content_type="image/png")
 
 
-import matplotlib.pyplot as plt
-import io
-from django.http import HttpResponse
-
+# Gráfico de Uso do Solo
 
 def generate_pie_chart_solo_usage(request):
     # Exemplo de dados
@@ -168,34 +165,3 @@ def get_solo_preservado_count():
         .order_by("area_vegetacao")
     )
     return list(queryset)
-
-
-# def dashboard(request):
-#     produtores = ProdutorRural.objects.all()
-#     fazendas = Fazenda.objects.all()
-#     total_fazendas = fazendas.count()
-#     area_total = fazendas.aggregate(Sum('area_total'))['area_total__sum']
-#     area_agricultavel = fazendas.aggregate(Sum('area_agricultavel'))['area_agricultavel__sum']
-#     area_vegetacao = fazendas.aggregate(Sum('area_vegetacao'))['area_vegetacao__sum']
-
-#     estado_count = fazendas.values('estado').annotate(count=Count('estado'))
-#     cultura_count = fazendas.values('culturas').annotate(count=Count('culturas'))
-#      # QuerySet para contagem de estados
-#     estado_count_queryset = Fazenda.objects.values('estado').annotate(count=Count('estado'))
-#     # Convertendo QuerySet para lista de dicionários
-#     estado_count = list(estado_count_queryset)
-#     context = {
-#         'estado_count': estado_count,
-#         'tipo_estado_count': type(estado_count).__name__,
-#     }
-
-#     return render(request, 'produtor/dashboard.html', {
-#         'produtores': produtores,
-#         'fazendas': fazendas,
-#         'total_fazendas': total_fazendas,
-#         'area_total': area_total,
-#         'area_agricultavel': area_agricultavel,
-#         'area_vegetacao': area_vegetacao,
-#         'estado_count': estado_count,
-#         'cultura_count': cultura_count,
-#     })
